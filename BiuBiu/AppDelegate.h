52987cfe0a2bdddd56e133a9ad384aae935722e8
邓钦元 <adof@PDDeng.local>
2018-03-01 14:15:58 +0800

    Initial Commit

---
+++ BiuBiu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BiuBiu
+//
+//  Created by 邓钦元 on 2018/3/1.
+//  Copyright © 2018年 乐派互娱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BiuBiu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BiuBiu
+//
+//  Created by 邓钦元 on 2018/3/1.
+//  Copyright © 2018年 乐派互娱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
